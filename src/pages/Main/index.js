b8229a5cddc7b383f31ec83e8d96bb5ddb0ad308
Bruno Vieira <bruno.vieira97@outlook.com>
2019-04-16 22:36:33 -0300

    Iniciado trabalho com rotas. Integração com o back-end iniciada. Criação da page Box.
    Issues:
     * Aplicação não compila devido à erro relacionado ao regenerator-runtime (no such file or directory)

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
-import logo from "../../assets/logo.svg";
-import "./styles.css";
+import logo from '../../assets/logo.svg';
+import './styles.css';
 
 export default class Main extends Component {
+	state = {
+		newBox: ''
+	}
+
+	handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const response = await api.post(
+			'boxes',
+			{
+				title: this.state.newBox
+			}
+		);
+
+		this.props.history.push(`/box/${response._id}`);
+	}
+
+	handleInputChange = (event) => {
+		this.setState(
+			{
+				newBox: event.target.value
+			}
+		);
+	}
+
 	render() {
 		return (
 			<div id = "main-container" >
-				<form action = "">
+				<form onSubmit = {this.handleSubmit}>
 					<img src = {logo} draggable = "false" alt = "" />
-					<input placeholder = "Crie seu Box" />
+					<input
+						placeholder = "Crie seu Box"
+						value = {this.state.newBox}
+						onChange = {this.handleInputChange}
+					/>
 					<button type = "submit">Criar</button>
 				</form>
 			</div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: ''
+	baseURL: 'https://bvieira-rocketbox-backend.herokuapp.com'
 });
 
+export default api;
